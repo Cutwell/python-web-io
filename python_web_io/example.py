04c5f8274ebd86c774d6d34f99957f6c6f0ed140
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2023-04-15 22:32:24 +0100

    initial commit

---
+++ python_web_io/example.py
@@ -0,0 +1,4 @@
+print("Welcome to my age calculator!")
+year = int(input("What year were you born?"))
+age = 2023 - year
+print(f"So you must be {age} years old!")
---
+++ python_web_io/main.py
@@ -0,0 +1,44 @@
+import python_web_io
+import argparse
+
+
+def main(filepath: str, title: str, icon: str, debug: bool):
+    """
+    Loads the contents of a script and executes it.
+
+    Arguments:
+        filepath (str): filepath to script / entrypoint.
+    """
+    with open(filepath, "r") as file:
+        script = file.read()
+
+    # save args to config / Cache
+    python_web_io.Cache.set("script", script)
+    python_web_io.Cache.set("title", title)
+    python_web_io.Cache.set("icon", icon)
+
+    # start the Flask server
+    python_web_io.app.run(debug=debug)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate a web UI to iteract with a Python script."
+    )
+    parser.add_argument("filepath", type=str, help="Script filepath (required).")
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Boolean switch to debug the Flask server (True if flagged) (optional).",
+    )
+    parser.add_argument(
+        "--title",
+        type=str,
+        help="Title for webapp (optional).",
+        default="Python Web IO",
+    )
+    parser.add_argument(
+        "--icon", type=str, help="Emoji webapp icon (optional).", default="🎯"
+    )
+    args = parser.parse_args()
+    main(filepath=args.filepath, title=args.title, icon=args.icon, debug=args.debug)
